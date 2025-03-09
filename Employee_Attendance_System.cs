1fad6c1cd741306a1efed0a51d215b1a7161f36c
KyuubiNepal04 <kyuubinepal@gmail.com>
2025-03-09 14:24:08 +0800

    Update Employee_Attendance_System.cs

--- Employee_Attendance_System.cs
+++ Employee_Attendance_System.cs
@@ -67,32 +67,3 @@ namespace OOP_GROUP_PROJECT
         }
     }
 }
-
-            //Create object of the Employee class
-            Employee emp = new Employee(EmployeeName, EmployeeID, EmployeeDept);
-            emp.DisplayEmployee();
-
-        }
-
-        class Employee
-        {
-            public string EmpName;
-            public int EmpId;
-            public string EmpDept;
-            
-            public Employee(string empName, int empId, string empDept)
-            {
-                EmpName = empName;
-                EmpId = empId;
-                EmpDept = empDept;
-            }
-
-            public void DisplayEmployee()  //Method to display employee details
-            {
-                Console.WriteLine($"Employee Name: {EmpName}");
-                Console.WriteLine($"Employee ID: {EmpId}");
-                Console.WriteLine($"Employee Department: {EmpDept}");
-            }
-        }
-    }
-}
